ff4f44cbc86ae7fb0631051088f03b580d63900c
atishayjain3107 <atishayjain3107@gmail.com>
2019-04-20 15:58:34 +0530

    second commit to aarogya by atishay

--- project_file1.cpp
+++ project_file1.cpp
@@ -41,3 +41,95 @@ class checkUp
 		}
 
 }c;
+
+class student
+{
+	long admno;
+	//order-> sname,mname,fname,addr,fno,mno,bgp;
+	//char sName[50],mName[50],fName[50],addr[50],fNo[10],mNo[10],bGp[3];
+	char data[7][50];
+	public:
+		void inputData();
+		void displayData(int);
+		int getAdmno()
+		{
+			return(admno);
+		}
+
+}s;
+
+//student database functions
+void studentDB();
+void SD_addRecord();
+void SD_deleteRecord();
+void SD_viewAll();
+void SD_modifyRecord();
+void SD_searchRecord();
+
+//medical checkup functions
+void MedicalCheckup();
+void MC_addRecord();
+void MC_searchRecord(long admn);
+void MC_viewAll();
+
+//report functions
+void Report();
+void R_addRecord();
+void R_searchRecord(long);
+void view_date(int sdate, int smonth, int syear,int edate, int emonth, int eyear);
+void view_month(int mon);
+void curDate(int&,int&,int&);
+
+//ui functions
+void init();
+void mainmenu();
+void box(int,int,int,int);
+void cursor(int,int,char*,COLORS);
+void setText(int,int,char*,COLORS,int);
+char* getText(int,int,char*,COLORS,int);
+long getNum(int,int,char*,COLORS);
+void plus(char* s);
+void clear();
+
+//////////////////////////////////////////
+/////////////MAIN FUNCTION////////////////
+//////////////////////////////////////////
+
+void main()
+{
+	clrscr();
+	window(1,1,80,25);
+	textbackground(BLACK);
+	textcolor(BLACK);
+	clrscr();
+	window(2,2,79,24);
+	textbackground(WHITE);
+	clrscr();
+	window(15,10,70,15);
+	textbackground(RED);
+	clrscr();
+	window(38,5,48,20);
+	textbackground(RED);
+	clrscr();
+	window(20,10,45,10);
+	gotoxy(20,10);
+	cout<<"€€€€€  €€€    €€€€€   €€€   €€€€€ €     € €€€€€"<<endl;
+	window(20,11,45,11);
+	gotoxy(20,11);
+	cout<<"€   €  €  €   €   €  €   €  €      €   €  €   €"<<endl;
+	window(20,12,45,12);
+	gotoxy(20,12);
+	cout<<"€   €  €  €   €   €  €   €  €       € €   €   €"<<endl;
+	window(20,13,45,13);
+	gotoxy(20,13);
+	cout<<"€€€€€  €€€    €€€€€  €   €  € €€€    €    €€€€€"<<endl;
+	window(20,14,45,14);
+	gotoxy(20,14);
+	cout<<"€   €  €  €   €   €  €   €  €   €    €    €   €"<<endl;
+	window(20,15,45,15);
+	gotoxy(20,15);
+	cout<<"€   €  €   €  €   €   €€€   €€€€€    €    €   €"<<endl;
+	getch();
+	init();
+	mainmenu();
+}
