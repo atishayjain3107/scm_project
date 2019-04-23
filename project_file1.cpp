e924eacf9b777c1fa4ede909139177f61ff5c719
Arnav1511 <meetarnavranjan@gmail.com>
2019-04-23 23:36:34 +0530

    fourth commit

--- project_file1.cpp
+++ project_file1.cpp
@@ -634,3 +634,53 @@ void Report()
 							view_month(6);
 						}
 						flag=0;
+						break;
+					case 4: ch=27;
+				 }
+		}
+	} while(ch!=27);
+}
+
+void report:: displayData()
+{
+	init();
+	window(6,6,26,6);
+	cout<<"Admission num-> "<<admNo;
+	window(6,7,26,7);
+	gotoxy(6,7);
+	cout<<date<<"-"<<month<<"-"<<year<<"\t";
+	setText(6,8,res,WHITE);
+	getch();
+}
+
+void report:: inputData()
+{
+	init();
+	admNo = getNum(6,6,"Enter admission num-> ",WHITE);
+	setText(6,7,"Remarks/Reason (under 40 characters) -> ",WHITE);
+	getText(6,8,"",res,WHITE);
+	curDate(date,month,year);
+}
+
+void curDate(int &date, int&month, int&year)
+{
+	time_t t=time(0); // time right now
+	struct tm *now=localtime(&t);
+	year=(now->tm_year+1900);
+	month=(now->tm_mon+1);
+	date=(now->tm_mday);
+}
+
+void R_addRecord() //disp mech
+{
+	fstream f1;
+	char ch;
+	f1.open("report.dat",ios::app|ios::binary);
+	while(1)
+	{
+		rep.inputData();
+		f1.write((char*)&rep,sizeof(rep));
+		init();
+		setText(5,5,"Press N to stop entering data and ",WHITE);
+		setText(5,6,"any other key to continue entering data",WHITE);
+		window(5,7,10,7);
